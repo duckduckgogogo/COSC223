0d1c4d2e9fd7938de7968bfc5e7da96d5799a396
Samantha Rydzewski <srydzewski21@amherst.edu>
2019-03-07 13:50:41 -0500

    Create ExperimentLRU.java

---
+++ ExperimentLRU.java
@@ -0,0 +1,56 @@
+public class ExperimentLRU {
+
+  public int length;
+  public ExperimentLRU(int l){
+    //length is the length of the sequence we want
+    length = l;
+  }
+
+  public double runUniformForSize(int cacheSize){
+    double sum = 0;
+    for (int i=0;i<100;i++){
+      UniformDis dis = new UniformDis(1000);
+      int[] requests = dis.generateUniform(length);
+      LRUCache cache = new LRUCache(cacheSize, requests);
+      double hitrate = cache.simulation();
+      //System.out.println(hitrate);
+      sum += hitrate;
+    }
+    return sum/100;
+
+  }
+
+  public double[] runUniform(){
+    double[] hitRates = new double[191];
+    for(int i=10; i<201;i++){
+      double avgHitRate = runUniformForSize(i);
+      hitRates[i-10] = avgHitRate;
+      System.out.println("done with uniform of size " + i);
+    }
+    return hitRates;
+  }
+
+  public double runZipfForSize(int cacheSize) {
+    double sum = 0;
+    for (int i=0;i<100;i++){
+      ZipfD dis = new ZipfD(1000);
+      int[] requests = dis.generateZipf(length);
+      LRUCache cache = new LRUCache(cacheSize, requests);
+      double hitrate = cache.simulation();
+      sum += hitrate;
+    }
+    return sum/100;
+
+  }
+
+  public double[] runZipf(){
+    double[] hitRates = new double[191];
+    for(int i=10; i<201;i++){
+      double avgHitRate = runZipfForSize(i);
+      hitRates[i-10] = avgHitRate;
+    }
+    return hitRates;
+  }
+
+
+}
