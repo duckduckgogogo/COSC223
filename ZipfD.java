d684e4e188ef0284a2cca054809cd746c969c6c2
Samantha Rydzewski <srydzewski21@amherst.edu>
2019-03-07 01:12:38 -0500

    Create ZipfD.java

---
+++ ZipfD.java
@@ -0,0 +1,87 @@
+import java.util.Random;
+public class ZipfD{
+  private int ram_size;
+  private Random gen;
+
+  public ZipfD(int ram){
+    ram_size = ram;
+    gen = new Random();
+  }
+
+  public int[] generateZipf(int l){
+    int[] requests = new int[l];
+    //calculating the denominator for probabilites
+    double sum = calcSum();
+    System.out.println("done with sum" + sum);
+
+    //calculating probabilities * l and length of master list
+    //this is to help fill the array toPickFrom
+    int[] probs = new int[ram_size];
+    int array_length = 0;
+    for (int i = 1; i<=ram_size;i++){
+      double numorator = 1.0/i;
+      //System.out.println(numorator);
+      //stem.out.println((numorator/sum) * l);
+      int numToEnter = (int)((numorator/sum) * l);
+      //System.out.println(numToEnter);
+      probs[i-1] = numToEnter;
+      array_length += numToEnter;
+      //System.out.println(numToEnter);
+
+    }
+    System.out.println("done with probs ");
+    System.out.println("Array length " + array_length);
+    
+    //creating master list of indexes to pick from for distribution
+    int[] toPickFrom = createToPickFrom(array_length, probs);
+    System.out.println("done with master list");
+
+    //shuffle
+    toPickFrom = shuffle(toPickFrom);
+    System.out.println("done with shuffling");
+
+    //fill requests
+    for(int i =0; i < l; i++){
+      int index = gen.nextInt(array_length);
+      requests[i] = toPickFrom[index];
+    }
+    System.out.println("done with requests");
+    return requests;
+
+  }
+
+  public double calcSum(){
+    double sum = 0;
+    for (int i =1; i<= ram_size; i++){
+        sum += 1.0/i;
+    }
+    return sum;
+
+  }
+
+  public int[] createToPickFrom(int array_length, int[] probs){
+    int[] toPickFrom = new int[array_length];
+    int start = 0;
+    for (int i =0; i<ram_size;i++){
+        for(int j = 0; j<probs[i]; j++){
+          toPickFrom[start+j] = i + 1;
+        }
+        start += probs[i];
+    }
+
+    return toPickFrom;
+
+  }
+
+  public int[] shuffle(int[] toPickFrom){
+    int array_length = toPickFrom.length;
+    for(int i =0; i < array_length; i++){
+      int toSwap = toPickFrom[i];
+      int index = gen.nextInt(array_length);
+      toPickFrom[i] = toPickFrom[index];
+      toPickFrom[index] = toSwap;
+    }
+    return toPickFrom;
+  }
+
+}
