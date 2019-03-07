303e6db3bd3896b7b5e2a2f719a49346401e6d3d
duckduckgogogo <44583752+duckduckgogogo@users.noreply.github.com>
2019-03-07 09:15:17 -0500

    Update CacheInterface.java

--- CacheInterface.java
+++ CacheInterface.java
@@ -1,1 +1,14 @@
-Hi
+public interface CacheInterface {
+
+  public void replace(Integer toAdd);
+
+public boolean checkCacheFull ();
+
+public boolean checkPresence (int input);
+
+public void replace (int input);
+
+public int simulation ();
+
+
+}
