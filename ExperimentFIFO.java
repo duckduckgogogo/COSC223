844a6a0dea61b55146d4d364c9b691f4c1b23434
Samantha Rydzewski <srydzewski21@amherst.edu>
2019-03-08 11:46:31 -0500

    Update ExperimentFIFO.java

--- ExperimentFIFO.java
+++ ExperimentFIFO.java
@@ -52,5 +52,27 @@ public class ExperimentFIFO {
     return hitRates;
   }
 
+  public double runGardForSize(int cacheSize) {
+    double sum = 0;
+    for (int i=0;i<100;i++){
+      GardnerDist dis = new GardnerDist(1000);
+      int[] requests = dis.generateGardner(length);
+      FIFOCache cache = new FIFOCache(cacheSize, requests);
+      double hitrate = cache.simulation();
+      sum += hitrate;
+    }
+    return sum/100;
+
+  }
+
+  public double[] runGardner(){
+    double[] hitRates = new double[191];
+    for(int i=10; i<201;i++){
+      double avgHitRate = runGardForSize(i);
+      hitRates[i-10] = avgHitRate;
+    }
+    return hitRates;
+  }
+
 
 }
